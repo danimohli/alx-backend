2af845bdbbe164514ec715d2f24d82adea3f41c8
danimohli <danimohlife@gmail.com>
2024-10-29 22:28:03 +0100

    lifo cache

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-LIFOCache module implementing a LIFO caching system.
-"""
+"""LIFOCache module implementing a LIFO caching system."""
 
 from base_caching import BaseCaching
 
@@ -16,7 +14,7 @@ class LIFOCache(BaseCaching):
         Initialize the cache and call the parent class's init method.
         """
         super().__init__()
-        self.last_key = None
+        self.last_key = None  # Track the most recently added key
 
     def put(self, key, item):
         """
@@ -26,14 +24,15 @@ class LIFOCache(BaseCaching):
             key: The key under which the item should be stored.
             item: The item to be stored in the cache.
 
-        If the cache exceeds MAX_ITEMS, the most recently
-        added item is discarded.
+        If the cache exceeds MAX_ITEMS,
+        the most recently added item is discarded.
         If either key or item is None, this method does nothing.
         """
         if key is not None and item is not None:
-            self.cache_data[key] = item
-            if key != self.last_key:
+
+            if key not in self.cache_data:
                 self.last_key = key
+            self.cache_data[key] = item
 
             if len(self.cache_data) > self.MAX_ITEMS:
                 del self.cache_data[self.last_key]
